1a996c56cd99a38cb5f98cdf0cba7155dffe3360
David Nyman <david.nyman@protonmail.com>
2023-10-04 11:19:34 +0200

    Add model

---
+++ HabitLogger/Models/HabitRecord.cs
@@ -0,0 +1,7 @@
+﻿namespace HabitLogger.Models;
+
+internal class HabitRecord
+{
+    public DateTime Date { get; set; }
+    public int Quantity { get; set; }
+}
--- HabitLogger/Program.cs
+++ HabitLogger/Program.cs
@@ -1,7 +1,11 @@
-﻿namespace HabitLogger;
+﻿using HabitLogger.Models;
+
+namespace HabitLogger;
 
 internal static class Program
 {
+    internal static List<HabitRecord> Records = new();
+
     static void Main()
     {
         MainMenu.Show();
